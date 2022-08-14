3c46b972fcc3e0ae08a22c5d5e5f71ceb8257042
Ofer Sadan <ofersadan85@gmail.com>
2022-08-14 23:39:24 +0300

    fix quotes

--- install.ps1
+++ install.ps1
@@ -9,10 +9,10 @@ $profile_parent = (get-item $PROFILE).Directory.FullName
 New-Item -ItemType Directory -Force -Path $profile_parent
 
 # Download theme
-$theme_src = https://raw.githubusercontent.com/ofersadan85/oh-my-posh-theme/main/ofersadan.omp.json
+$theme_src = "https://raw.githubusercontent.com/ofersadan85/oh-my-posh-theme/main/ofersadan.omp.json"
 $theme_dest = Join-Path -Path $profile_parent -ChildPath "ofersadan.omp.json"
 Invoke-WebRequest $theme_src -OutFile $theme_dest
 
 # Download profile
-$profile_src = https://raw.githubusercontent.com/ofersadan85/oh-my-posh-theme/main/profile.ps1
+$profile_src = "https://raw.githubusercontent.com/ofersadan85/oh-my-posh-theme/main/profile.ps1"
 Invoke-WebRequest $theme_src -OutFile $PROFILE
